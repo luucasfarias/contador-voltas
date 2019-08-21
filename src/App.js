960ba572ecab502842bd6b78dc73d5b8cf064412
lucas <luucasfarias21@gmail.com>
2019-08-20 21:15:57 -0300

    logica da aplicação, funcionalidade de componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const MostrarVoltas = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>
+      {props.voltas}<br />
+      Nº Voltas
+    </p>
+  )
+}
+
+const MostrarTempoMedio = (props) => {
+  const tempoMedio = props.tempoMedio;
+  const minutos = Math.round(tempoMedio / 60);
+  const segundos = tempoMedio % 60;
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos;
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos;
+  return (
+    <p>
+      {`${minutosStr}:${segundosStr}`} <br />
+      Tempo médio por volta
+    </p>
+  )
+}
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+function App() {
+  const [numVoltas, setNumVoltas] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [tempoMedio, setTempoMedio] = useState(0);
+
+  useEffect(() => {
+    let timer = null;
+    if (running) {
+      timer = setInterval(() => {
+        setTempoMedio(old => old + 1);
+      }, 1000);
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    }
+  }, [running]);
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1);
+  }
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1);
+  }
+
+  const reset = () => {
+    setNumVoltas(0);
+    setTempoMedio(0);
+  }
+
+  return (
+    <div>
+      <MostrarVoltas voltas={numVoltas}></MostrarVoltas>
+
+      <Button text="+" onClick={increment}></Button>
+      <Button text="-" onClick={decrement}></Button>
+
+      {
+        numVoltas > 0 &&
+        <MostrarTempoMedio tempoMedio={Math.round(tempoMedio / numVoltas)}></MostrarTempoMedio>
+      }
+
+      <Button onClick={toggleRunning} text="Iniciar"></Button>
+      <Button onClick={reset} text="Reiniciar"></Button>
     </div>
   );
 }
